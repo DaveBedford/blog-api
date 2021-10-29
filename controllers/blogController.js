30ad9bc004f0ea08fbafaf9d22e1d7c854b5e768
Dave Bedford <davewbedford@gmail.com>
2021-10-30 00:47:54 +0100

    patch will not work

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -65,6 +65,23 @@ const post = (req, res) => {
 |----------------------------------------------------------------------
 */
 const updatePost = async (req, res) => {
+	try {
+		// Get input
+		const { title, body, description, image, featured } = req.body;
+
+		console.log(title)
+	
+		// Query for the specific blog post and update with inputs
+		const post = await Post.findByIdAndUpdate(req.params.id, { title, body, description, image, featured });
+		if (!post) return res.status(404).json({ error: "Error updating post." });
+		res.status(201).json({ message: "Successfully updated post." });
+	  } catch (err) {
+		console.log(err);
+		return next(err);
+	  }
+	}
+
+
 	// const post = await Post.findById(req.params.id);
 	// if (req.body.title != null) {
 	// 	post.title = req.body.title;
@@ -94,27 +111,27 @@ const updatePost = async (req, res) => {
 
 
 	//console.log(req.params.id);
-	console.log(req.body.title);
+// 	console.log(req.body.title);
 
-	const post = await Post.findById(req.params.id);
-	try {
-		post.title = req.body.title;
-		post.body = req.body.body;
-		post.description = req.body.description;
-		post.image = req.body.image;
-		post.featured = req.body.featured;
+// 	const post = await Post.findById(req.params.id);
+// 	try {
+// 		post.title = req.body.title;
+// 		post.body = req.body.body;
+// 		post.description = req.body.description;
+// 		post.image = req.body.image;
+// 		post.featured = req.body.featured;
 
-		// post.save()
-		// .then((result) => {
-		// 	res.status(201).json(post);
-		// })
-		// .catch((err) => {
-		// 	res.status(400).json({ message: err.message });
-		// });
-	} catch(err) {
-		res.status(400).json({ message: err.message });
-	}
-};
+// 		// post.save()
+// 		// .then((result) => {
+// 		// 	res.status(201).json(post);
+// 		// })
+// 		// .catch((err) => {
+// 		// 	res.status(400).json({ message: err.message });
+// 		// });
+// 	} catch(err) {
+// 		res.status(400).json({ message: err.message });
+// 	}
+// };
 
 /*
 |----------------------------------------------------------------------
